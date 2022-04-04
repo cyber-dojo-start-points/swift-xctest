242996e539107035e94631286c2d97eeb25ec728
Jon Reid <jon@qualitycoding.org>
2022-04-04 11:30:29 -0700

    Avoid setUp - not needed for most exercises

--- start_point/hikertests.swift
+++ start_point/hikertests.swift
@@ -1,20 +1,8 @@
 import XCTest
 
 class HikerTests: XCTestCase {
-    var hiker: Hiker!
-
-    override func setUp() {
-        super.setUp()
-        hiker = Hiker()
-    }
-
-    override func tearDown() {
-        hiker = nil
-        super.tearDown()
-    }
-
     func testLife_the_universe_and_everything() {
-        XCTAssertEqual(hiker.answer(), 42)
+        XCTAssertEqual(Hiker().answer(), 42)
     }
 }
 
